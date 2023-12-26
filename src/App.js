98dbe769950c95d70ac5b37e4649c63c8520a95e
Zhiqin4724 <77992515+Zhiqin4724@users.noreply.github.com>
2023-12-25 23:07:44 -0500

    bruh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './nav-bar/Nav-Bar.js';
+import Apropos from './a-propos/A-Propos.js';
+import Contact from './Contact/contact.js';
+import Equipe from './Equipe/equipe.js';
+import About from './About/about.js';
+import Commandite from './Commandite/commandite.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="container">
+    <NavBar></NavBar>
+    <Apropos></Apropos>
+    <About></About>
+    <Equipe></Equipe>
+    <Commandite></Commandite>
+    <Contact></Contact>
+    </div></>
   );
 }
 
---
+++ src/Equipe/equipe.js
@@ -0,0 +1,16 @@
+import './equipe.css';
+
+function Equipe() {
+    return (
+
+      <div className="container-equipe">
+        <div className="logo-container">
+            <div id="">
+
+            </div>
+        </div>
+      </div>
+    );
+  }
+
+  export default Equipe;
