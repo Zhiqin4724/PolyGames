24792ac34c7cd163b1dc12432877d9e79e6690b1
Zhiqin <rzhiryin@hotmail.com>
2023-12-30 11:16:44 -0500

    commanditaire

--- src/Commandite/commandite.js
+++ src/Commandite/commandite.js
@@ -1,4 +1,8 @@
 import './commandite.css';
+import ubisoft from './../logo/ubisoft.png'
+import doublestalion from './../logo/pfNpTEu7_400x400.png'
+import eidos from './../logo/eidos-montreal-logo.jpg'
+import unity from './../logo/unity.png'
 
 function Commandite() {
     return (
@@ -6,7 +10,10 @@ function Commandite() {
       <div className="container-commandite">
         <div className="commanditaire-title">Nos Commanditaires</div>
         <div className='commanditaire-list'>
-          <div className='commanditaire-box'></div>
+            <img src={ubisoft} alt='' className='commanditaire-image'></img>
+            <img src={doublestalion} alt='' className='commanditaire-image'></img>
+            <img src={eidos} alt='' className='commanditaire-image'></img>
+            <img src={unity} alt='' className='commanditaire-image'></img>
         </div>
       </div>
     );
