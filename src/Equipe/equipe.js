10ebb72905a2fbfdbeb2657825102984aef49095
Zhiqin4724 <77992515+Zhiqin4724@users.noreply.github.com>
2023-12-30 23:42:29 -0500

    fix the variants bug

--- src/Equipe/equipe.js
+++ src/Equipe/equipe.js
@@ -1,4 +1,5 @@
 import './equipe.css';
+import React, { useState } from 'react';
 import cube from './../logo/CUBE_logoDiscord.png'
 import gamejam from './../logo/gamejam_square.png'
 import atelier from './../logo/creative-team.png'
@@ -23,39 +24,72 @@ const variants = {
 }
 
 function Equipe() {
+  const [hasAnimated, setHasAnimated] = useState(false);
+
+  const handleAnimationComplete = () => {
+    setHasAnimated(true);
+  };
     return (
 
       <div className="container-equipe">
         <div className='equipe-title-container'>
           <img></img>
-          <motion.div className='equipe-title' variants={variants} initial="initial" whileInView="animate">Notre Equipe</motion.div>
-          <motion.div className='equipe-title-2' variants={variants} initial="initial" whileInView="animate">Ipsum Lorem</motion.div>
+          <motion.div className='equipe-title' variants={variants}
+          initial="initial"
+          animate={hasAnimated ? "animate" : "initial"}
+          whileInView={hasAnimated ? undefined : "animate"}
+          onAnimationComplete={handleAnimationComplete}>Notre Equipe</motion.div>
+          <motion.div className='equipe-title-2' variants={variants}
+          initial="initial"
+          animate={hasAnimated ? "animate" : "initial"}
+          whileInView={hasAnimated ? undefined : "animate"}
+          onAnimationComplete={handleAnimationComplete}>Ipsum Lorem</motion.div>
           <img></img>
         </div>
         <motion.div className='equipe-cards-container'>
           
-            <motion.div className='equipe-cards-1' variants={variants} initial="initial" whileInView="animate">
+            <motion.div className='equipe-cards-1' variants={variants}
+          initial="initial"
+          animate={hasAnimated ? "animate" : "initial"}
+          whileInView={hasAnimated ? undefined : "animate"}
+          onAnimationComplete={handleAnimationComplete}>
               <img src={gamejam} alt='gamejam' className='equipe-gamejam-logo'></img>
             </motion.div>
           
           
-            <motion.div className='equipe-cards-2' variants={variants} initial="initial" whileInView="animate">
+            <motion.div className='equipe-cards-2' variants={variants}
+          initial="initial"
+          animate={hasAnimated ? "animate" : "initial"}
+          whileInView={hasAnimated ? undefined : "animate"}
+          onAnimationComplete={handleAnimationComplete}>
               <img src={atelier} alt='atelier' className='equipe-atelier-logo'></img>
             </motion.div>
           
           
-            <motion.div className='equipe-cards-3' variants={variants} initial="initial" whileInView="animate">
+            <motion.div className='equipe-cards-3' variants={variants}
+          initial="initial"
+          animate={hasAnimated ? "animate" : "initial"}
+          whileInView={hasAnimated ? undefined : "animate"}
+          onAnimationComplete={handleAnimationComplete}>
               <img src={gamedev} alt='gamedev' className='equipe-gamedev-logo'></img>
               <img src={manette} alt='manette' className='equipe-manette-logo'></img>
             </motion.div>
           
           
-            <motion.div className='equipe-cards-4' variants={variants} initial="initial" whileInView="animate">
+            <motion.div className='equipe-cards-4' variants={variants}
+          initial="initial"
+          animate={hasAnimated ? "animate" : "initial"}
+          whileInView={hasAnimated ? undefined : "animate"}
+          onAnimationComplete={handleAnimationComplete}>
               <img src={cube} alt='cube' className='equipe-cube-logo'></img>
             </motion.div>
           
           
-            <motion.div className='equipe-cards-5' variants={variants} initial="initial" whileInView="animate">
+            <motion.div className='equipe-cards-5' variants={variants}
+          initial="initial"
+          animate={hasAnimated ? "animate" : "initial"}
+          whileInView={hasAnimated ? undefined : "animate"}
+          onAnimationComplete={handleAnimationComplete}>
               <img src={concours} alt='concours' className='equipe-concours-logo'></img>
             </motion.div>
           
--- src/portfolio/portfolio.js
+++ src/portfolio/portfolio.js
@@ -1,6 +1,5 @@
 import './portfolio.css';
-import gamejam from './../logo/gamejam_square.png'
-import cube from './../logo/CUBE_logoDiscord.png'
+import React, { useState } from 'react';
 import atelier from './../logo/Poly Games Atelier Unity A2018.jpg'
 import gamedev from './../logo/Poly Games Jeu VR_AR.jpg'
 import visite from './../logo/Poly Games Unity.jpg'
@@ -21,19 +20,32 @@ const variants = {
     }
   }
 function Portfolio() {
+  const [hasAnimated, setHasAnimated] = useState(false);
+
+  const handleAnimationComplete = () => {
+    setHasAnimated(true);
+  };
   return (
     <>
     <div className='portfolio-container'>
         <div className='portfolio-titre'>Lorem Ipsum</div>
         <div className='portfolio-list'>
-            <motion.div className='portfolio-card-1' variants={variants} initial="initial" whileInView="animate">
+            <motion.div className='portfolio-card-1' variants={variants}
+          initial="initial"
+          animate={hasAnimated ? "animate" : "initial"}
+          whileInView={hasAnimated ? undefined : "animate"}
+          onAnimationComplete={handleAnimationComplete}>
                 <div className='portfolio-image-container'>
                 <img src={atelier} alt='' className='portfolio-image'></img>
                 </div>
                 <div className='portfolio-titre-image'>Atelier</div>
                 <div className='portfolio-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
             </motion.div>
-            <motion.div className='portfolio-card-2' variants={variants} initial="initial" whileInView="animate">
+            <motion.div className='portfolio-card-2' variants={variants}
+          initial="initial"
+          animate={hasAnimated ? "animate" : "initial"}
+          whileInView={hasAnimated ? undefined : "animate"}
+          onAnimationComplete={handleAnimationComplete}>
             <div className='portfolio-image-container'>
                 <img src={prix} alt='' className='portfolio-image'></img>
                 </div>
@@ -42,14 +54,22 @@ function Portfolio() {
             </motion.div>
         </div>
         <div className='portfolio-list'>
-            <motion.div className='portfolio-card-1' variants={variants} initial="initial" whileInView="animate">
+            <motion.div className='portfolio-card-1' variants={variants}
+          initial="initial"
+          animate={hasAnimated ? "animate" : "initial"}
+          whileInView={hasAnimated ? undefined : "animate"}
+          onAnimationComplete={handleAnimationComplete}>
             <div className='portfolio-image-container'>
                 <img src={visite} alt='visite' className='portfolio-image'></img>
                 </div>
                 <div className='portfolio-titre-image'>Visite Industriel</div>
                 <div className='portfolio-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
             </motion.div>
-            <motion.div className='portfolio-card-2' variants={variants} initial="initial" whileInView="animate">
+            <motion.div className='portfolio-card-2' variants={variants}
+          initial="initial"
+          animate={hasAnimated ? "animate" : "initial"}
+          whileInView={hasAnimated ? undefined : "animate"}
+          onAnimationComplete={handleAnimationComplete}>
             <div className='portfolio-image-container'>
                 <img src={gamedev} alt='' className='portfolio-image'></img>
             </div>
